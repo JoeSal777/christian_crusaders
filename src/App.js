fa107f91710661df25be0bf3b3e6fa6c72489e4c
JoeSalem <larkai.joseph@gmail.com>
2023-04-16 19:01:33 +0000

    react router works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Landingpage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Default from "./components/Default";
+
+class App extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Landingpage/>} />
+          <Route path="/contact" element={<Contact/>} />
+          < Route path="*" element={<Default/>} />
+        </Routes>       
+    </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <h3>hello from the Landing Page</h3>
+            </div>
+        );
+    }
+}
